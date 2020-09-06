57df8525c9e1f769a5ca166fcaea31510f67a00d
Elena Leib <eleib@berkeley.edu>
2020-09-05 18:05:45 -0700

    Create Week3_warmup_starter.R

---
+++ Week3_warmup_starter.R
@@ -0,0 +1,60 @@
+# QuACK 2020 
+# Week 3 - Logic and Data Cleaning
+# Warm-up
+# Authors: Willa & Elena
+
+options(stringsAsFactors=FALSE)
+
+#### Group practice ####
+# the goal is for you guys to work together to figure out some info about this dataset. 
+# try to get all the info from R (ie. don't open the csv file in excel).
+########################
+
+#### 1.  Set your directory ####
+
+# i. What is your current working directory?
+
+
+# ii. Set your wd to the folder with the data and R script (if it isn't already the current wd)
+
+
+
+
+#### 2. Load your data ####
+# Load in penguin.csv
+
+
+#### 3. Get some general info about the dataframe ####
+# You can use any commands to get this info. Try to use R commands, don't just open up the dataframe and look. That said, feel free to look at the data frame and in the global environment to confirm the output of the R commands and your understanding.
+
+# i. How many columns are there? 
+
+
+# ii. What are the column names
+
+
+# iii. How many rows?
+
+
+# iv. What are the datatypes in this dataframe? 
+
+
+# v. Are there any that would be better as factors? Change them!
+
+
+# vi. What are a few other ways you can inspect your data without using R commands? Explain as many as you can think of.
+
+
+
+#### 4. Learn more about each column ####
+
+# i. Type a command to get descriptive stats (e.g., mean, median, sd) for each column
+
+
+# ii. Do any columns have missing values (i.e., NAs)? If so, which ones and how many NAs in each column?
+
+
+#### Challenge: Create a new column called cuteness.rating. Use what you learned last time to give them all a 10! ####
+
+
+
